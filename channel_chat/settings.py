6b912fa4d0d24897a4905f135acdc77b5be2ee1d
kimnt <you@example.com>
2022-12-27 11:20:32 +0700

    consumers websocket

--- channel_chat/settings.py
+++ channel_chat/settings.py
@@ -72,7 +72,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'channel_chat.wsgi.application'
 
-ASGI_APPLICATION = "channel_chat.asgi.application"
+ASGI_APPLICATION = 'channel_chat.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379, '/2')],
+        },
+    },
+}
 
 
 # Database
